06e496d65291bead458449ef482d31ab6c0463d1
chien <nguyencaochien03@gmail.com>
2023-07-12 23:25:51 +0700

    update1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 
 import './App.scss';
+import Header from './components/Header/Header';
+import { Link } from 'react-router-dom';
 
 const App = () => {
 
   return (
 
     <div className="app-container">
-      hello world
+      <Header/>
+      <div>
+        test Link
+        <div>
+          <button>
+            <Link to = "/Users">Go to user page</Link>
+          </button>
+          <button>
+            <Link to = "/Admins">Go to admin page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link to='/' className='nav-link'>Home</Link>
+            <Link to='/Users' className='nav-link'>Users</Link>
+            <Link to='/Admins' className='nav-link'>Admin</Link>
+
+            {/* <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/Users">Users</Nav.Link>
+            <Nav.Link href="/Admins">Admin</Nav.Link> */}
+            
+          </Nav>
+            <NavDropdown title="Settings" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Log out
+              </NavDropdown.Item>
+              
+            </NavDropdown>
+          <Nav>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;

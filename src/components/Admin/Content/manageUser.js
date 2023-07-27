8e4595a8da222b74c675cf11cd0a60d15f53a7b5
chien <nguyencaochien03@gmail.com>
2023-07-27 22:55:21 +0700

    createModal

---
+++ src/components/Admin/Content/manageUser.js
@@ -0,0 +1,20 @@
+import ModalCreateUser from "./ModalCreateUser";
+
+const ManageUser = (props) => {
+  return (
+    <div className="manage-user-container">
+      <div className="title">Manager User</div>
+      <div className="User-content">
+        <div>
+          <button>Add new user</button>
+        </div>
+        <div>
+          table users
+          <ModalCreateUser />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ManageUser;
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import User from "./components/User/User";
 import Admin from "./components/Admin/Admin";
 import HomePage from "./components/Home/HomePage";
+import DashBoard from "./components/Admin/Content/dashBoard";
+import ManageUser from "./components/Admin/Content/manageUser";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -19,7 +21,10 @@ root.render(
           <Route index element={<HomePage />} />
           <Route path="/Users" element={<User />} />
         </Route>
-        <Route path="/Admins" element={<Admin />} />
+        <Route path="/Admins" element={<Admin />}>
+          <Route index element={<DashBoard />} />
+          <Route path="manage-user" element={<ManageUser />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </Provider>
